598180f4cb77d460991ce74c1e1b759027889e93
fervillalbag <21fervillalbag@gmail.com>
2022-02-28 18:55:39 -0300

    feat: theme option

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,10 @@
 import React from 'react'
-import {
-  Button,
-  Flex,
-  Heading,
-  Input,
-  useColorMode,
-  useColorModeValue
-} from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react'
 
 const Home: React.FC = () => {
-  const { toggleColorMode } = useColorMode()
-  const formBackground = useColorModeValue('gray.100', 'gray.700')
-
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background={formBackground} p={12} rounded={6}>
-        <Heading mb={6}>Log in</Heading>
-        <Input
-          placeholder="fervillalbag@gmail.com"
-          variant="filled"
-          mb={3}
-          type="email"
-        />
-        <Input
-          placeholder="fervillalbag@gmail.com"
-          variant="filled"
-          mb={6}
-          type="password"
-        />
-        <Button colorScheme="teal" mb={6}>
-          Log in
-        </Button>
-        <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
-      </Flex>
+      <Heading backgroundColor="secondary">Hello</Heading>
     </Flex>
   )
 }
